ccb40f7ef281c5f3a036fbb55f97b840a18c50cc
Harshprajapati1234 <harshjeetn.prajapati@gmail.com>
2024-10-16 16:29:04 +0530

    vv

---
+++ Day1/day1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    int a;
+    cin >> a;
+    if (a >= 1)
+    {
+        cout << a << " is a positive Number";
+    }
+       else if (a == 0)
+        {
+            cout << a << " is a Neutral Number";
+        }
+    else
+    {
+
+        cout << a << " is a Nagative Number";
+    }
+}
