b73ce0eb591ba79320395fe896dd2569d5469509
Harshprajapati1234 <harshjeetn.prajapati@gmail.com>
2024-10-16 16:09:35 +0530

    day 2

---
+++ Day 2/Day2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    int day;
+    cin >> day;
+
+    switch (day)
+    {
+    case 1:
+        cout << "press 1 for English";
+        break;
+    case 2:
+        cout << "press 2 for Hindi";
+        break;
+    case 3:
+        cout << "press 3 for Gujrati";
+        break;
+    }
+}
