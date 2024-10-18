c217bc9e67ceff038c574f2470fea91efed653b7
Harshprajapati1234 <harshjeetn.prajapati@gmail.com>
2024-10-18 15:41:28 +0530

    new

---
+++ Day 3 task/Q2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+main (){
+
+    int b= 10;
+    while (b < 0)
+    {
+        cout <<  b  << "";
+
+        b++;
+    } 
+
+
+}
---
+++ Day 3 task/Q3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int i = 0;
+    int n;
+    cin >> n;
+
+    while (i < n)
+
+    {
+        cout << i << "     ";
+        i++;
+        
+    }
+}
