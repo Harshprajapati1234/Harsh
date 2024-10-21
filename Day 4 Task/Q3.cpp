2a3ac5653f75adccde9520ade2d27c1a3b1d44f4
Harshprajapati1234 <harshjeetn.prajapati@gmail.com>
2024-10-21 16:37:24 +0530

    f

--- Day 4 Task/Q3.cpp
+++ Day 4 Task/Q3.cpp
@@ -1,1 +1,23 @@
 #include <iostream>
+using namespace std;
+
+main()
+{
+
+   int n;
+
+   cout << "enter Number - ";
+   cin >>n;
+   int i=1;
+
+do{
+    
+    cout << n << "x" << i << "=" << n*i << endl;
+    i++;
+
+}while( i<=10);
+
+// cout<<i;
+
+
+}
