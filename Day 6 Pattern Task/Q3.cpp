f0a3b3bd5c6aa7e750fd1b2c3f87e8c6f04d67df
Harshprajapati1234 <harshjeetn.prajapati@gmail.com>
2024-10-22 17:09:47 +0530

    i

--- Day 6 Pattern Task/Q3.cpp
+++ Day 6 Pattern Task/Q3.cpp
@@ -4,13 +4,13 @@ using namespace std;
 main()
 {
 
-    for (int i = 1; i <= 5; i++)
+    for (int i = 5; i >= 1; i--)
     {
 
-        for (int j = i; j >= 1; j--)
+        for (int j = i; j <=5; j++)
         {
 
-            cout << j << " ";
+            cout << i << " ";
         }
         cout << endl;
     }return 0;
